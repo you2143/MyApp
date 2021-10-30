b2565c913d5fb74cf769687df575d4bd9ab94b2e
you2143 <your.00719@gmail.com>
2021-10-30 14:30:57 +0900

    first commit

---
+++ MyApp.Infrastructure/Users/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using MyApp.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Infrastructure.Users
+{
+    class UserRepository : IUserRepository
+    {
+        public User Find(UserId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
